a786f65db93149667838e8105024f43df92dd904
Artur Sulej <artur@shedul.com>
2020-08-13 00:22:02 +0200

    Config enabling 1-Wire

--- firmware/config/config.exs
+++ firmware/config/config.exs
@@ -12,6 +12,8 @@ config :firmware, target: Mix.target()
 
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
+config :nerves, :firmware, fwup_conf: "config/fwup.conf"
+
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
 
